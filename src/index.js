b96c60f9c52d6605282469cfe238c8698982ea35
Christian_Polanco <chrispolanco@sbcglobal.net>
2020-01-21 19:59:47 -0800

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const form = document.getElementById("create-task-form");
+  const newtaskfield = document.getElementById("new-task-description");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const newTask = document.createElement("li");
+    newTask.innerHTML = newtask.value;
+    taskList.appendChild(newTask);
+    event.target.reset();
+  };
 });
+
